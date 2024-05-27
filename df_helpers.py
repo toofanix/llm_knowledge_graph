6857ddd6c9b969753ff9dfaa83ee2200745f3380
jigarbandaria <jbandaria@om1.com>
2024-05-27 11:27:46 +0000

    defined function to convert graph to df

--- df_helpers.py
+++ df_helpers.py
@@ -40,3 +40,10 @@ def df2Graph(dataframe: pd.DataFrame, model=None)->list:
     # Flattent the list of lists to a single list
     concept_list = np.concatenate(results).ravel().tolist()
     return concept_list
+
+def graph2Df(nodes_list) -> pd.DataFrame:
+    graph_dataframe = pd.DataFrame(nodes_list).replace(" ", np.nan)
+    graph_dataframe = graph_dataframe.dropna(subset=["node_1", "node_2"])
+    graph_dataframe["node_1"] = graph_dataframe["node_1"].apply(lambda x: x.lower())
+    graph_dataframe["node_2"] = graph_dataframe["node_2"].apply(lambda x: x.lower())
+    return graph_dataframe
