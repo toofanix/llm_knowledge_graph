b2fc327a4af5a1676c4695f760dad7a9cbfa499e
jigarbandaria <jbandaria@om1.com>
2024-05-27 10:53:07 +0000

    defined function to make graph

--- prompts.py
+++ prompts.py
@@ -35,5 +35,47 @@ def extractConcepts(prompt: str, metadata={}, model="mistral-openorca:latest"):
     return result
 
 
+def graphPrompt(input: str, metadata={}, model='mistral-openorca:latest'):
+    if model == None:
+        model = "mistral-openorca:latest"
+    model_info = client.show(model_name=model)
+    print(chalk.blue(model_info))
+
+    SYS_PROMPT = (
+        "You are a network graph maker who extracts terms and their relations from a given context. "
+        "You are provided with a context chunk (delimited by ```) Your task is to extract the ontology "
+        "of terms mentioned in the given context. These terms should represent the key concepts as per the context. \n"
+        "Thought 1: While traversing through each sentence, Think about the key terms mentioned in it.\n"
+            "\tTerms may include object, entity, location, organization, person, \n"
+            "\tcondition, acronym, documents, service, concept, etc.\n"
+            "\tTerms should be as atomistic as possible\n\n"
+        "Thought 2: Think about how these terms can have one on one relation with other terms.\n"
+            "\tTerms that are mentioned in the same sentence or the same paragraph are typically related to each other.\n"
+            "\tTerms can be related to many other terms\n\n"
+        "Thought 3: Find out the relation between each such related pair of terms. \n\n"
+        "Format your output as a list of json. Each element of the list contains a pair of terms"
+        "and the relation between them, like the follwing: \n"
+        "[\n"
+        "   {\n"
+        '       "node_1": "A concept from extracted ontology",\n'
+        '       "node_2": "A related concept from extracted ontology",\n'
+        '       "edge": "relationship between the two concepts, node_1 and node_2 in one or two sentences"\n'
+        "   }, {...}\n"
+        "]"
+    )
+
+    USER_PROMPT = f"context: ```{input}``` \n\n output: "
+
+    response, _ = client.generate(model_name=model, system=SYS_PROMPT, prompt=USER_PROMPT)
+    try:
+        result = json.loads(response)
+        result = [dict(item, **metadata) for item in result]
+    except:
+        print("\n\nERROR ### Here is the buggy response: ", response, "\n\n")
+        result = None
+    return result
+
+    
+
 
     
