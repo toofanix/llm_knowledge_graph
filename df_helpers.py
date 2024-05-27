7370cea4e64395092a3fe24db6c90c6c033be966
jigarbandaria <jbandaria@om1.com>
2024-05-27 11:21:58 +0000

    defined function to convert df to concept list

--- df_helpers.py
+++ df_helpers.py
@@ -15,3 +15,16 @@ def documents2Dataframe(documents) -> pd.Dataframe:
     df = pd.DataFrame(rows)
     return df
 
+def df2ConceptList(dataframe: pd.DataFrame) -> List:
+    results = dataframe.apply(
+        lambda row: extractConcepts(
+            row.text, {"chunk_id": row.chunk_id, "type":"concept"}
+        ),
+        axis=1
+    )
+    results = results.dropna()
+    results = results.reset_index(drop=True)
+
+    # Flatten the list of lists to single list
+    concept_list = np.concatenate(results).ravel().tolist()
+    return concept_list
