1fe49d2954b7a2d910072da0413773c0ac88fd3e
jigarbandaria <jbandaria@om1.com>
2024-05-27 11:17:35 +0000

    function to convert document to dataframe

--- df_helpers.py
+++ df_helpers.py
@@ -1,4 +1,17 @@
 import uuid
 import pandas as pd
 import numpy as np
-from 
+from prompts import extractConcepts, graphPrompt
+
+def documents2Dataframe(documents) -> pd.Dataframe:
+    rows = []
+    for chunk in documents:
+        row = {
+            "text": chunk.page_content,
+            **chunk.metadata,
+            "chunk_id": uuid.uuid4().hex
+        }
+        rows = rows + row
+    df = pd.DataFrame(rows)
+    return df
+
